84a256c0aecd504d4c03f5e61cb749a4226d094c
Teepluss <teepluss@gmail.com>
2015-10-22 07:20:03 +0000

    change package model

--- src/Teepluss/Up2ServiceProvider.php
+++ src/Teepluss/Up2ServiceProvider.php
@@ -1,98 +1,104 @@
-<?php namespace Teepluss\Up2;
+<?php 
 
-use Illuminate\Support\ServiceProvider;
-use Teepluss\Up2\Attachments\Eloquent\Provider as AttachmentProvider;
+namespace Teepluss\Up2;
 
 use Imagine\Image\Box;
 use Imagine\Image\Point;
+use Illuminate\Support\ServiceProvider;
+use Teepluss\Up2\Attachments\Eloquent\Provider as AttachmentProvider;
 
+class Up2ServiceProvider extends ServiceProvider 
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-class Up2ServiceProvider extends ServiceProvider {
+    /**
+     * Register package.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/config.php' => config_path('up2.php')
+        ], 'config');
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+        $this->publishes([
+            __DIR__.'/../database/migrations/' => database_path('migrations')
+        ], 'migrations');
+    }
 
-	/**
-	 * Register package.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('teepluss/up2');
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerAttachmentProvider();
+        $this->registerUploader();
+        $this->registerUp2();
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->registerAttachmentProvider();
-		$this->registerUploader();
-		$this->registerUp2();
-	}
+    /**
+     * Register attachment provider.
+     *
+     * @return void
+     */
+    protected function registerAttachmentProvider()
+    {
+        $this->app['up2.attachment'] = $this->app->share(function($app)
+        {
+            $model = $app['config']->get('up2::attachments.model');
 
-	/**
-	 * Register attachment provider.
-	 *
-	 * @return void
-	 */
-	protected function registerAttachmentProvider()
-	{
-		$this->app['up2.attachment'] = $this->app->share(function($app)
-		{
-			$model = $app['config']->get('up2::attachments.model');
+            return new AttachmentProvider($model);
+        });
+    }
 
-			return new AttachmentProvider($model);
-		});
-	}
+    /**
+     * Register uploader adapter.
+     *
+     * @return void
+     */
+    public function registerUploader()
+    {
+        $this->app['up2.uploader'] = $this->app->share(function($app)
+        {
+            return new UploaderManager($app);
+        });
+    }
 
-	/**
-	 * Register uploader adapter.
-	 *
-	 * @return void
-	 */
-	public function registerUploader()
-	{
-		$this->app['up2.uploader'] = $this->app->share(function($app)
-		{
-			return new UploaderManager($app);
-		});
-	}
+    /**
+     * Register core class.
+     *
+     * @return void
+     */
+    protected function registerUp2()
+    {
+        $this->app['up2'] = $this->app->share(function($app)
+        {
+            $app['up2.loaded'] = true;
 
-	/**
-	 * Register core class.
-	 *
-	 * @return void
-	 */
-	protected function registerUp2()
-	{
-		$this->app['up2'] = $this->app->share(function($app)
-		{
-			$app['up2.loaded'] = true;
+            //s(get_class_methods($app['up2.uploader']));
 
-			//s(get_class_methods($app['up2.uploader']));
+            //sd($app['up2.uploader']->getDefaultDriver());
 
-			//sd($app['up2.uploader']->getDefaultDriver());
+            return new Up2($app['config'], $app['up2.attachment'], $app['up2.uploader']);
+        });
+    }
 
-			return new Up2($app['config'], $app['up2.attachment'], $app['up2.uploader']);
-		});
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('attach', 'up2');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('attach', 'up2');
+    }
 
 }
