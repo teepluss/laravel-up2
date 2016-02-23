71170670e1251c9ed387dc272f988b64e4b2c621
Teepluss <admin@jquerytips.com>
2016-02-23 14:26:31 +0700

    add helper up()

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+use Teepluss\Up2\Facades\Up2;
+
+if (! function_exists('up')) {
+    /**
+     * New instance of UP2.
+     *
+     * @return \Teepluss\Up2\Facades\Up2
+     */
+    function up()
+    {
+        return new Up2()
+    }
+}
